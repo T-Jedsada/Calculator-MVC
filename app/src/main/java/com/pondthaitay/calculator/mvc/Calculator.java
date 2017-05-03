7191958cf108cc2baff03fdc482308b8e227f06f
Jedsada Tiwongvorakul <jt@20scoops.com>
2017-05-04 01:39:05 +0700

    :zap: first commit

---
+++ app/src/main/java/com/pondthaitay/calculator/mvc/Calculator.java
@@ -0,0 +1,20 @@
+package com.pondthaitay.calculator.mvc;
+
+public class Calculator {
+
+    public int plus(int x, int y) {
+        return x + y;
+    }
+
+    public int minus(int x, int y) {
+        return x - y;
+    }
+
+    public int multiply(int x, int y) {
+        return x * y;
+    }
+
+    public int divide(int x, int y) {
+        return x / y;
+    }
+}
---
+++ app/src/test/java/com/pondthaitay/calculator/mvc/CalculatorTest.java
@@ -0,0 +1,31 @@
+package com.pondthaitay.calculator.mvc;
+
+import org.junit.Test;
+
+import static org.hamcrest.CoreMatchers.is;
+import static org.hamcrest.MatcherAssert.assertThat;
+
+public class CalculatorTest {
+    private Calculator calculator = new Calculator();
+
+    @Test
+    public void plus() throws Exception {
+        assertThat(calculator.plus(5,5), is(10));
+    }
+
+    @Test
+    public void minus() throws Exception {
+        assertThat(calculator.minus(5,5), is(0));
+    }
+
+    @Test
+    public void multiply() throws Exception {
+        assertThat(calculator.multiply(5,5), is(25));
+    }
+
+    @Test
+    public void divide() throws Exception {
+        assertThat(calculator.divide(5,5), is(1));
+    }
+}
+
